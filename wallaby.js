fdc746468b55a3c62890278849ba3eeb5efe2b81
john <2849964+johnqiuwan@users.noreply.github.com>
2023-04-07 00:31:39 -0700

    clean folder

---
+++ wallaby.js
@@ -0,0 +1,13 @@
+module.exports = function (wallaby) {
+  return {
+    files: [
+      //   'src/**/*.js'
+    ],
+
+    tests: [
+      // 'src/cmpStore/**/*.spec.{ts,tsx,js,jsx}'
+    ],
+    // for node.js tests you need to set env property as well
+    // https://wallabyjs.com/docs/integration/node.html
+  };
+};
