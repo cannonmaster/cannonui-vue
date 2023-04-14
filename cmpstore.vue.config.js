35e9392cddd4b614c22088f1df0053d41c42d7ac
john <2849964+johnqiuwan@users.noreply.github.com>
2023-04-14 03:15:57 -0700

    alpha release prepare

--- cmpstore.vue.config.js
+++ cmpstore.vue.config.js
@@ -4,6 +4,7 @@ import { visualizer } from 'rollup-plugin-visualizer';
 import { defineConfig } from 'vite';
 import dts from 'vite-plugin-dts';
 export default defineConfig((config) => ({
+  publicDir: false,
   build: {
     lib: {
       // Could also be a dictionary or array of multiple entry points
