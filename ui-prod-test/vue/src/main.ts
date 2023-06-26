6624cd5b5b5ff4a23372e2f2811335ef969c56a5
cannon <cannonmaster666@gmail.com>
2023-06-26 15:43:19 -0700

    clean files

--- ui-prod-test/vue/src/main.ts
+++
@@ -1,6 +0,0 @@
-import { createApp } from 'vue'
-import './style.css'
-import App from './App.vue'
-import CannonUI from '@cannonui/vue'
-import '@cannonui/vue/style.css'
-createApp(App).use(CannonUI).mount('#app')
