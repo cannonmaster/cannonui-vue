9e678dfe0bd3adf6c9c44d7b383c3a2caaca6dfd
cannon <cannonmaster666@gmail.com>
2023-06-27 15:38:58 -0700

    fix url rediction on git page

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,10 @@
-import { defineConfig } from 'astro/config';
+import mdx from '@astrojs/mdx';
 import preact from '@astrojs/preact';
 import react from '@astrojs/react';
+import vue from '@astrojs/vue';
+import { defineConfig } from 'astro/config';
 import path from 'path';
 import { fileURLToPath } from 'url';
-import mdx from '@astrojs/mdx';
-import vue from '@astrojs/vue';
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 // https://astro.build/config
@@ -13,22 +13,24 @@ const __dirname = path.dirname(__filename);
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://cannonmaster.github.io',
+  base: '/cannonui-vue',
   vite: {
     resolve: {
       alias: [
         {
           find: '@',
-          replacement: path.resolve(__dirname, 'src/'),
-        },
-      ],
+          replacement: path.resolve(__dirname, 'src/')
+        }
+      ]
     },
     css: {
       preprocessorOptions: {
         scss: {
-          additionalData: `@import "@/styles/variables.scss";`,
-        },
-      },
-    },
+          additionalData: `@import "@/styles/variables.scss";`
+        }
+      }
+    }
   },
   integrations: [
     // Enable Preact to support Preact JSX components.
@@ -36,7 +38,7 @@ export default defineConfig({
     // Enable React for the Algolia search component.
     react(),
     mdx(),
-    vue(),
+    vue()
   ],
-  site: `http://astro.build`,
+  site: `http://astro.build`
 });
